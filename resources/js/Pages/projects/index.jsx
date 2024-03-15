8e2416ae06159ef0551b56c6b4b381522a632618
Yaine Garcerant <yainegarcerant964@gmail.com>
2024-03-15 16:32:15 -0500

    Formato de Documento

--- resources/js/Pages/projects/index.jsx
+++ resources/js/Pages/projects/index.jsx
@@ -50,7 +50,7 @@ const index = ({ auth }) => {
                 }
             >
                 <div className="py-12">
-                    <div className=" sm:px-6 lg:px-8">
+                    <div className="sm:px-6 lg:px-8">
                         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                                 <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -60,69 +60,66 @@ const index = ({ auth }) => {
 
                             <Head title="Categories" />
                             <div className="grid grid-rows-4 grid-flow-col gap-4 p-4 sm:p-6 lg:p-8">
-                                <form onSubmit={submit}>
-                                    <div className="grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 gap-4">
-                                        <div className="mx-0">
-                                            <input
-                                                value={data.name}
-                                                onChange={(e) =>
-                                                    setData({
-                                                        data,
-                                                        name: e.target.value,
-                                                    })
-                                                }
-                                                type="text"
-                                                placeholder="Title"
-                                                autoFocus
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            />
-                                            <InputError
-                                                message={errors.name}
-                                                className="mt-2"
-                                            />
+                                <form
+                                    onSubmit={submit}
+                                    className="sm:grid sm:grid-cols-2 sm:gap-4"
+                                >
+                                    <div className="mx-0">
+                                        <input
+                                            value={data.name}
+                                            onChange={(e) =>
+                                                setData({
+                                                    ...data,
+                                                    name: e.target.value,
+                                                })
+                                            }
+                                            type="text"
+                                            placeholder="Title"
+                                            autoFocus
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                        <InputError
+                                            message={errors.name}
+                                            className="mt-2"
+                                        />
 
-                                            <input
-                                                value={data.url}
-                                                onChange={(e) =>
-                                                    setData({
-                                                        ...data,
-                                                        url: e.target.value,
-                                                    })
-                                                }
-                                                type="text"
-                                                placeholder="URL"
-                                                autoFocus
-                                                className="mt-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            />
-                                            <InputError
-                                                message={errors.url}
-                                                className="mt-2"
-                                            />
-                                        </div>
-
-
-                                            <textarea
-                                                value={data.descripcion}
-                                                onChange={(e) =>
-                                                    setData({
-                                                        ...data,
-                                                        descripcion:
-                                                            e.target.value,
-                                                    })
-                                                }
-                                                placeholder="Body"
-                                                rows="3"
-                                                className="rows-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            />
-                                            <InputError
-                                                message={errors.descripcion}
-                                                className="mt-2"
-                                            />
-
+                                        <input
+                                            value={data.url}
+                                            onChange={(e) =>
+                                                setData({
+                                                    ...data,
+                                                    url: e.target.value,
+                                                })
+                                            }
+                                            type="text"
+                                            placeholder="URL"
+                                            autoFocus
+                                            className="mt-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                        <InputError
+                                            message={errors.url}
+                                            className="mt-2"
+                                        />
                                     </div>
+                                    <textarea
+                                        value={data.descripcion}
+                                        onChange={(e) =>
+                                            setData({
+                                                ...data,
+                                                descripcion: e.target.value,
+                                            })
+                                        }
+                                        placeholder="Body"
+                                        rows="3"
+                                        className="rows-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-3 sm:mt-0"
+                                    />
+                                    <InputError
+                                        message={errors.descripcion}
+                                        className="mt-2"
+                                    />
 
-                                    <div className="flex flex-row">
-                                        <div className="basis-1/4 mr-2">
+                                    <div className="flex flex-col sm:flex-row sm:space-x-4">
+                                        <div className="sm:w-1/2">
                                             <select
                                                 value={data.categories_id}
                                                 onChange={(e) =>
@@ -134,7 +131,13 @@ const index = ({ auth }) => {
                                                 }
                                                 className="mt-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                             >
-                                                <option value="" disabled selected>Selecione Una Categoria</option>
+                                                <option
+                                                    value=""
+                                                    disabled
+                                                    selected
+                                                >
+                                                    Selecione Una Categoria
+                                                </option>
                                                 {categories?.map((category) => (
                                                     <option
                                                         key={category.id}
@@ -150,7 +153,7 @@ const index = ({ auth }) => {
                                             />
                                         </div>
 
-                                        <div className="basis-1/4 mx-2">
+                                        <div className="sm:w-1/2 ">
                                             <input
                                                 value={data.image}
                                                 onChange={(e) =>
@@ -169,8 +172,10 @@ const index = ({ auth }) => {
                                                 className="mt-2"
                                             />
                                         </div>
+                                    </div>
 
-                                        <div className="basis-1/4 mx-2">
+                                    <div className="flex flex-col sm:flex-row sm:space-x-4">
+                                        <div className="sm:w-1/2">
                                             <input
                                                 value={data.start_date}
                                                 onChange={(e) =>
@@ -191,7 +196,7 @@ const index = ({ auth }) => {
                                             />
                                         </div>
 
-                                        <div className="basis-1/4 ml-2">
+                                        <div className="sm:w-1/2">
                                             <input
                                                 value={data.end_date}
                                                 onChange={(e) =>
@@ -212,7 +217,6 @@ const index = ({ auth }) => {
                                             />
                                         </div>
                                     </div>
-
                                     <div className="sm:col-span-2">
                                         <PrimaryButton
                                             className="mt-4 flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
